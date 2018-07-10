5184cab5d8147c9b0278c995cb29f873ba1fe360
Simon Lindsay <simon@iseek.biz>
2018-07-10 20:46:18 +0930

    Initial commit.

---
+++ test/test-app/web/index.php
@@ -0,0 +1,11 @@
+
+<!doctype html>
+<html>
+	<head>
+		<title>Hello World!</title>
+	</head>
+	<body>
+		<h1>Hello World!</h1>
+		<p><?php echo(implode(' ', scandir(__DIR__))) ?></p>
+	</body>
+</html>
